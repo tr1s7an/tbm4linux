26bd074cebaf5705f72e29ad57fdeb2bde3bce74
tr1s7an <50695187+tr1s7an@users.noreply.github.com>
2024-05-04 09:57:47 +0800

    20240504

--- tbm4linux.py
+++ tbm4linux.py
@@ -38,19 +38,53 @@ def check_version(checkver_dict):
     return new_version
 
 
-def download_asset(url, asset_name):
-    print("downloading...")
-    file_response = requests.get(url)
+def install(config_file):
+    _, formatted_config = read_config(config_file)
+    asset_name = formatted_config["architecture"][ARCHITECTURE]["asset_name"]
+    if not os.path.exists(f"{CACHE_PATH}/{id}"):
+        os.mkdir(f"{CACHE_PATH}/{id}")
+    os.chdir(f"{CACHE_PATH}/{id}")
+
+    print("\tdownloading...", end="")
+    file_response = requests.get(formatted_config["architecture"][ARCHITECTURE]["url"])
     with open(asset_name, "wb") as file:
         file.write(file_response.content)
-    print(f"{asset_name} downloaded successfully")
+    print("ok", end="")
+
+    if formatted_config["architecture"][ARCHITECTURE]["extract"]:
+        print("...extracting...", end="")
+        try:
+            os.system(f"{EXTRACT_SCRIPT_PATH} {asset_name}")
+            print("ok")
+        except:
+            print("failed!")
+            return
+    else:
+        print()
+    for k, v in formatted_config["architecture"][ARCHITECTURE]["folder"].items():
+        dst = os.path.join(FOLDER_PATH, v)
+        print(f"\t{k} -> {dst}")
+        if os.path.exists(dst):
+            if input(f"\tconfirm that {dst} is going to be deleted(y/n): ") == "y":
+                shutil.rmtree(dst)
+            else:
+                print(f"\t{dst} not updated")
+                continue
+        shutil.copytree(k, dst)
+    for k, v in formatted_config["architecture"][ARCHITECTURE]["bin"].items():
+        dst = os.path.join(BINARY_PATH, v)
+        print(f"\t{k} -> {dst}")
+        shutil.copy2(k, dst)
+        os.chmod(dst, 0o755)
+    os.chdir(cwd)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Linux Binary Manager")
     parser.add_argument("ids", nargs="+", help="binary id")
-    parser.add_argument("--install", "-i", action="store_true", help="Install the binary")
     parser.add_argument("--check", "-c", action="store_true", help="Check for updates")
+    parser.add_argument("--install", "-i", action="store_true", help="Install the binary")
+    parser.add_argument("--update", "-u", action="store_true", help="Check for updates and install")
     args = parser.parse_args()
 
     if args.ids[0] == "*":
@@ -59,48 +93,24 @@ if __name__ == "__main__":
         ids = args.ids
 
     cwd = os.getcwd()
-    for id in ids:
-        print(id)
+    for idx, id in enumerate(ids):
+        print(f"({idx+1:03}/{len(ids):03})[{id}]: ", end="")
         config_file = f"{BUCKET_PATH}/{id}.json"
         if os.path.exists(config_file):
-            if args.check:
+            if args.check or args.update:
                 config, formatted_config = read_config(config_file)
                 old_version = config["version"]
                 new_version = check_version(config["checkver"])
                 if new_version != old_version:
-                    print(f"found update: {old_version} -> {new_version}")
+                    print(f"<{old_version}> -> <{new_version}>")
                     config["version"] = new_version
                     update_config(config, config_file)
+                    if args.update:
+                        install(config_file)
                 else:
-                    print(f"current version: {old_version}; no update found")
+                    print(f"<{old_version}>")
             if args.install:
-                config, formatted_config = read_config(config_file)
-                asset_name = formatted_config["architecture"][ARCHITECTURE]["asset_name"]
-                if not os.path.exists(f"{CACHE_PATH}/{id}"):
-                    os.mkdir(f"{CACHE_PATH}/{id}")
-                os.chdir(f"{CACHE_PATH}/{id}")
-                download_asset(formatted_config["architecture"][ARCHITECTURE]["url"], asset_name)
-                if "." in asset_name:
-                    try:
-                        os.system(f"{EXTRACT_SCRIPT_PATH} {asset_name}")
-                    except:
-                        print(f"skip {id}")
-                        continue
-                for k, v in formatted_config["architecture"][ARCHITECTURE]["folder"].items():
-                    dst = os.path.join(FOLDER_PATH, v)
-                    print(k, " -> ", dst)
-                    if os.path.exists(dst):
-                        if input(f"confirm that {dst} is going to be deleted(y/n): ") == "y":
-                            shutil.rmtree(dst)
-                        else:
-                            print(f"{dst} not updated")
-                            continue
-                    shutil.copytree(k, dst)
-                for k, v in formatted_config["architecture"][ARCHITECTURE]["bin"].items():
-                    dst = os.path.join(BINARY_PATH, v)
-                    print(k, " -> ", dst)
-                    shutil.copy2(k, dst)
-                    os.chmod(dst, 0o755)
-                os.chdir(cwd)
+                install(config_file)
         else:
             print(f"config file {config_file} not found")
+        print(f"({idx+1:03}/{len(ids):03})[{id}]: done")
